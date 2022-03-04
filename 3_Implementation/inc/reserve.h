470eff0f35a83207105560e7e2e9d98bda8afda2
dhaya <70699827+dhaya007@users.noreply.github.com>
2022-03-04 12:30:37 +0530

    Add files via upload

---
+++ 3_Implementation/inc/reserve.h
@@ -0,0 +1,20 @@
+/**
+ * @file reserve.h
+ * @author DHAYANITHI M
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef RESERVE_H_
+#define RESERVE_H_
+
+#define size 2
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#endif
